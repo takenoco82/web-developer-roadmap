68589e52d7078c6094a3e1b8bf932f1767c720d6
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-08-25 23:32:20 +0900

    [add] エントリーポイントの作成

---
+++ tutorial-webpack/src/js/app.js
@@ -0,0 +1,11 @@
+import { addCalc } from "./modules/addition-calculator";
+import { taxCalc } from "./modules/tax-calculator";
+
+const item1Price = 400;
+const item2Price = 600;
+
+const totalPrice = addCalc(item1Price, item2Price);
+const tax = 1.08;
+const priceIncludeTax = taxCalc(totalPrice, tax);
+
+console.log(`税込価格=${priceIncludeTax}`);
