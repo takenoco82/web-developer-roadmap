6d0c20da9c8d16970f417b141208b60e59c4f5dd
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-09-15 20:34:41 +0900

    [fix] ユーザ詳細画面を修正

--- sandbox_app/src/pages/users/_id.vue
+++ sandbox_app/src/pages/users/_id.vue
@@ -3,8 +3,66 @@
     <h1>This is The user page</h1>
     <v-container>
       <p>{{user_id}}</p>
-      <p>{{username}}</p>
-      <p>{{email}}</p>
+      <v-card
+        max-width="375"
+        class="mx-auto"
+      >
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/lists/ali.png"
+          height="300px"
+          dark
+        >
+          <v-row class="fill-height">
+            <v-card-title>
+              <v-btn dark icon @click="$router.go(-1)">
+                <v-icon>mdi-chevron-left</v-icon>
+              </v-btn>
+
+              <div class="flex-grow-1"></div>
+
+              <v-btn dark icon class="mr-4">
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+            </v-card-title>
+
+            <div class="flex-grow-1"></div>
+
+            <v-card-title class="white--text pl-12 pt-12">
+              <div class="display-1 pl-12 pt-12">{{username}}</div>
+            </v-card-title>
+          </v-row>
+        </v-img>
+
+        <v-list>
+          <v-list-item @click="">
+            <v-list-item-icon>
+              <v-icon color="indigo">mdi-phone</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>(650) 555-1234</v-list-item-title>
+              <v-list-item-subtitle>Mobile</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-icon>
+              <v-icon>mdi-message-text</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item @click="">
+            <v-list-item-icon>
+              <v-icon color="indigo">mdi-email</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{email}}</v-list-item-title>
+              <v-list-item-subtitle>Personal</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
     </v-container>
   </div>
 </template>
