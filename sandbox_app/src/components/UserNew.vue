0a7198674772ca9cce9a519fee156cd0509be7d9
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-09-16 19:48:49 +0900

    [add] ユーザ登録ダイアログを追加

---
+++ sandbox_app/src/components/UserNew.vue
@@ -0,0 +1,116 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn class="mx-2" fab dark color="indigo" v-on="on">
+          <v-icon dark>mdi-plus</v-icon>
+        </v-btn>
+      </template>
+      <v-form @submit.prevent="save">
+        <v-card>
+          <v-card-actions>
+            <v-btn icon @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+            <v-row align="center" justify="end">
+              <v-btn class="mr-3" color="primary" type="submit">Save</v-btn>
+            </v-row>
+          </v-card-actions>
+          <v-card-title>
+            <v-text-field
+              v-model="username"
+              label="user name"
+              :error-messages="errors.username"
+              required
+            ></v-text-field>
+          </v-card-title>
+          <v-card-text>
+            <v-text-field
+              v-model="email"
+              label="e-mail"
+              :error-messages="errors.email"
+              required
+            >
+            </v-text-field>
+          </v-card-text>
+        </v-card>
+      </v-form>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+import Sandbox from "sandbox";
+
+export default {
+  data() {
+    return {
+      dialog: false,
+      valid: true,
+      username: "",
+      email: "",
+      // エラーメッセージ格納オブジェクト
+      errors: {
+        username: "",
+        email: ""
+      }
+    }
+  },
+  methods: {
+    save() {
+      console.log("click save");
+
+      // API実行前にエラーをクリアする
+      this.resetErrors();
+
+      this.postUser(this.username, this.email).then(user => {
+        console.log(user);
+
+        // 後処理
+        this.cleanUp();
+      }).catch(error => {
+        // TODO ホントは400 BadRequest の場合だけやるとか色々ある
+        const errResponse = JSON.parse(error.response.text);
+        // APIからかえってきたエラーメッセージを設定
+        errResponse.errors.map(error => {
+          this.errors[error.field] = error.message;
+        });
+        console.log(this.errors);
+      });
+    },
+    postUser(username, email) {
+      const defaultClient = Sandbox.ApiClient.instance;
+
+      const apiInstance = new Sandbox.UsersApi();
+      const body = new Sandbox.WriteUser(); // WriteUser | Created user object
+      body.username = username
+      body.email = email
+
+      return apiInstance.postUser(body).then(data => {
+        console.log('API called successfully. UsersApi.postUser');
+        return data;
+      }).catch(error => {
+        console.error(error);
+        throw error;
+      });
+    },
+    resetErrors() {
+      for (const field of Object.keys(this.errors)) {
+        this.errors[field] = "";
+      }
+      console.log(this.errors);
+    },
+    cleanUp() {
+      // dataを初期化して、ダイアログを閉じる
+      this.username = "";
+      this.email = "";
+      this.resetErrors();
+
+      this.dialog = false;
+
+      // 一覧を最新化する
+      this.$emit("refresh");
+    }
+  }
+}
+</script>
---
+++ sandbox_app/src/pages/user_list.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <h1>This is The users page</h1>
+    <v-container>
+      <div>
+        <v-data-table
+          :headers="headers"
+          :items="users"
+          :page.sync="page"
+          :items-per-page="itemsPerPage"
+          hide-default-footer
+          class="elevation-1"
+          @page-count="pageCount = $event"
+        >
+          <template v-slot:item.user_id="{ item }">
+            <span class="pr-2">
+              <v-avatar color="indigo" size="24">
+                <span class="white--text">{{ item.avatar_text }}</span>
+              </v-avatar>
+            </span>
+            <router-link v-bind:to=item.url>{{ item.user_id }}</router-link>
+          </template>
+        </v-data-table>
+        <div class="text-center pt-2">
+          <v-pagination v-model="page" :length="pageCount"></v-pagination>
+        </div>
+      </div>
+    </v-container>
+
+    <UserNew v-on:refresh="udpateUsers" />
+  </div>
+</template>
+
+<script>
+import Sandbox from "sandbox";
+import UserNew from "@/components/UserNew";
+
+export default {
+  components: {
+    UserNew
+  },
+  data() {
+    return {
+      users: [],
+      page: 1,
+      pageCount: 0,
+      itemsPerPage: 10,
+      headers: [
+        { text: 'id', value: 'user_id' },
+        { text: 'user name', value: 'username' },
+        { text: 'e-mail address', value: 'email' },
+      ],
+    }
+  },
+  methods: {
+    getUsers() {
+      const defaultClient = Sandbox.ApiClient.instance;
+
+      // Configure API key authorization: api_key
+      const api_key = defaultClient.authentications['api_key'];
+      api_key.apiKey = "YOUR API KEY"
+      // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+      //api_key.apiKeyPrefix['api_key'] = "Token"
+
+      const apiInstance = new Sandbox.UsersApi()
+      const opts = {};
+      return apiInstance.getUsers(opts).then(data => {
+        console.log('API called successfully. Returned data: ' + data);
+        return data.users;
+      }).catch(error => {
+        console.error(error);
+        throw error;
+      });
+    },
+    udpateUsers() {
+      this.getUsers().then(users => {
+      this.users = users.map(user => {
+        user.url = `/user_list/${user.user_id}`;
+        // username = 'Yvette Ebert' -> avatar_text = 'YE'
+        user.avatar_text = user.username.split(" ").map(item => item.substring(0, 1)).join("");
+        return user;
+      });
+    });
+    }
+  },
+  // created で arrow関数を使うと メソッドが this で呼び出せなくなる？
+  created() {
+    this.udpateUsers();
+  }
+}
+</script>
