0123b002d9feb84a16c72a2f08be189bb63a73d9
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-09-26 21:14:04 +0900

    [fix] マテリアルデザインアイコンを使えるようにした

--- sandbox_app/src/plugins/vuetify.js
+++ sandbox_app/src/plugins/vuetify.js
@@ -1,3 +1,5 @@
+// [Icons — Vuetify.js](https://vuetifyjs.com/ja/customization/icons#install-material-icons)
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
@@ -5,6 +7,6 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: "mdi"
+    iconfont: "md"
   }
 });
