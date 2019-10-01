7c9540e385062b9246df900d5a7d7fca3b173d41
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-10-02 01:03:10 +0900

    [fix] prettierの設定を追加

--- tutorial-eslint/src/views/About.vue
+++ tutorial-eslint/src/views/About.vue
@@ -3,3 +3,24 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      message: 'aha'
+    }
+  },
+
+  methods: {
+    func1() {
+      console.log('aha')
+      console.log('ihi')
+      const aha = 1
+      if (aha) {
+        console.log('ufu')
+      }
+    }
+  }
+}
+</script>
