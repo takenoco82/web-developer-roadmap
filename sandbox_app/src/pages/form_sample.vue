d0c0a8376527ba7f4ec94eeb5678b10f76a0f30f
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-12-16 21:04:34 +0900

    [add] とりあえず入力フォームを作成

---
+++ sandbox_app/src/pages/form_sample.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-row>
+        <v-col cols="12" sm="6" md="3">
+          <v-text-field label="name" v-model="field.name"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          <v-text-field label="email" v-model="field.email"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6" md="3">
+          <v-btn @click="save">save</v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      field: {
+        name: null,
+        email: null
+      }
+    };
+  },
+  methods: {
+    save() {
+      console.log(this.field);
+    }
+  }
+};
+</script>
