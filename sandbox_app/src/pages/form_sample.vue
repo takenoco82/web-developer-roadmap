0fe417c2a6ed28566fe47e2413fee73602ef0e1c
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-12-16 21:18:26 +0900

    [fix] 配列の項目を追加

--- sandbox_app/src/pages/form_sample.vue
+++ sandbox_app/src/pages/form_sample.vue
@@ -10,8 +10,21 @@
         </v-col>
       </v-row>
       <v-row>
+        <v-container>
+          <p>hobby</p>
+          <v-btn fab x-small color="primary" @click="addHobby">
+            <v-icon dark>mdi-plus</v-icon>
+          </v-btn>
+          <v-row v-for="(item, index) in field.hobbies" :key="index">
+            <v-col>
+              <v-text-field label="hobby" :value="item"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-row>
+      <v-row>
         <v-col cols="12" sm="6" md="3">
-          <v-btn @click="save">save</v-btn>
+          <v-btn @click="save" color="primary">save</v-btn>
         </v-col>
       </v-row>
     </v-form>
@@ -24,13 +37,17 @@ export default {
     return {
       field: {
         name: null,
-        email: null
+        email: null,
+        hobbies: ["aha", "ihi"]
       }
     };
   },
   methods: {
     save() {
       console.log(this.field);
+    },
+    addHobby() {
+      this.field.hobbies.push("");
     }
   }
 };
