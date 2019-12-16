dcc90aae8e20769e16dc8dcdb6fb8b340e41b81d
Daisuke TAKEUCHI <takenoco82@gmail.com>
2019-12-16 22:00:34 +0900

    [fix] コンポーネント化

---
+++ sandbox_app/src/components/InputForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-form>
+    <v-row>
+      <v-col cols="12" sm="6" md="3">
+        <v-text-field label="name" v-model="fields.name"></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-text-field label="email" v-model="fields.email"></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-container>
+        <p>hobby</p>
+        <v-btn fab x-small color="primary" @click="addHobby">
+          <v-icon dark>mdi-plus</v-icon>
+        </v-btn>
+        <v-row v-for="(item, index) in fields.hobbies" :key="index">
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              label="hobby"
+              prepend-icon="delete"
+              :value="item"
+              @input="updateHobby(index)"
+              @click:prepend="removeHobby(index)"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" md="3">
+        <v-btn @click="$emit('save-item', fields)" color="primary">save</v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script>
+export default {
+  props: {
+    initialData: Object
+  },
+  data() {
+    return {
+      fields: null
+    };
+  },
+  methods: {
+    initilize() {
+      this.fields = JSON.parse(JSON.stringify(this.initialData));
+    },
+    addHobby() {
+      this.fields.hobbies.push("");
+    },
+    updateHobby(index) {
+      console.log("updateHobby", event.target.value);
+      console.log(this.fields.hobbies);
+      this.fields.hobbies[index] = event.target.value;
+    },
+    removeHobby(index) {
+      console.log("removeHobby clicked", index);
+      this.fields.hobbies.splice(index, 1);
+    }
+  },
+  created() {
+    this.initilize();
+  }
+};
+</script>
--- sandbox_app/src/pages/form_sample.vue
+++ sandbox_app/src/pages/form_sample.vue
@@ -1,44 +1,16 @@
 <template>
   <v-container>
-    <v-form>
-      <v-row>
-        <v-col cols="12" sm="6" md="3">
-          <v-text-field label="name" v-model="fields.name"></v-text-field>
-        </v-col>
-        <v-col cols="12" sm="6" md="3">
-          <v-text-field label="email" v-model="fields.email"></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-container>
-          <p>hobby</p>
-          <v-btn fab x-small color="primary" @click="addHobby">
-            <v-icon dark>mdi-plus</v-icon>
-          </v-btn>
-          <v-row v-for="(item, index) in fields.hobbies" :key="index">
-            <v-col>
-              <v-text-field
-                label="hobby"
-                prepend-icon="delete"
-                :value="item"
-                @input="updateHobby(index)"
-                @click:prepend="removeHobby(index)"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-row>
-      <v-row>
-        <v-col cols="12" sm="6" md="3">
-          <v-btn @click="save" color="primary">save</v-btn>
-        </v-col>
-      </v-row>
-    </v-form>
+    <input-form :initialData="fields" @save-item="save"></input-form>
   </v-container>
 </template>
 
 <script>
+import InputForm from "@/components/InputForm.vue";
+
 export default {
+  components: {
+    "input-form": InputForm
+  },
   data() {
     return {
       fields: {
@@ -49,20 +21,8 @@ export default {
     };
   },
   methods: {
-    save() {
-      console.log(this.field);
-    },
-    addHobby() {
-      this.fields.hobbies.push("");
-    },
-    updateHobby(index) {
-      console.log("updateHobby", event.target.value);
-      console.log(this.fields.hobbies);
-      this.fields.hobbies[index] = event.target.value;
-    },
-    removeHobby(index) {
-      console.log("removeHobby clicked", index);
-      this.fields.hobbies.splice(index, 1);
+    save(form) {
+      console.log(form);
     }
   }
 };
